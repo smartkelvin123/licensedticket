27aec25968f80d047fefa3bb8b2f34808dbeb0c7
unknown <smartkelvin66@gmail.com>
2024-10-29 14:36:56 +0100

    update

--- src/licence.sol
+++ src/licence.sol
@@ -1,44 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-
 /**
  * @title LicenseManager
  * @dev cretaing a smart contract caled LicenseManager to manage licenses, the cost is 1 ether, owner can ask for free
- * 
- * 
+ *
+ *
  */
-
 contract LicenseManager {
     address private owner;
     address[] private licensed;
-    mapping (address => bool) private licenseOwner;
-
+    mapping(address => bool) private licenseOwner;
 
     constructor() {
         owner = msg.sender;
     }
 
-
-    function buyLicenese() public payable  {
-         require(msg.value == 1 ether  || msg.sender == owner, "send 1ether to buy a license. owners can ask for free");
-         licensed.push(msg.sender);
-         licenseOwner[msg.sender] = true;
+    function buyLicenese() public payable {
+        require(msg.value == 1 ether || msg.sender == owner, "send 1ether to buy a license. owners can ask for free");
+        licensed.push(msg.sender);
+        licenseOwner[msg.sender] = true;
     }
 
     function checkLicense() public view returns (bool) {
         return licenseOwner[msg.sender];
     }
 
-
-  //  the blockchain is deterimistic, the varaibles are known, using them to calculate random element is not good
+    //  the blockchain is deterimistic, the varaibles are known, using them to calculate random element is not good
     function winLicense() public payable returns (bool) {
         require(msg.value >= 0.01 ether && msg.value <= 0.5 ether, "send between 0.01 and 0.5 ether to try your luck");
-        uint maxThreshold = uint ((msg.value / 1e16));
+        uint256 maxThreshold = uint256((msg.value / 1e16));
 
-        // bad design principle by using blockhass
-        uint algorithm = uint (keccak256(abi.encodePacked(uint256(msg.value), msg.sender, uint(1337), blockhash(block.number-1))));
-        uint pickedNumber = algorithm % 100;
+        // bad design principle by using blockhash
+        uint256 algorithm = uint256(
+            keccak256(abi.encodePacked(uint256(msg.value), msg.sender, uint256(1337), blockhash(block.number - 1)))
+        );
+        uint256 pickedNumber = algorithm % 100;
         if (pickedNumber < maxThreshold) {
             licenseOwner[msg.sender] = true;
         }
@@ -49,24 +46,29 @@ contract LicenseManager {
         //checking if the refund address was registered as a licensed user
         require(licenseOwner[msg.sender] == true, "you are not a licensed user");
 
-        for (uint i = 0; i < licensed.length; i++) {
+
+         // solution for the re-entrance attack..  always uppdadate state before making any external call
+           // Update state before making any external call
+          //  licenseOwner[msg.sender] = false;
+          for (uint256 i = 0; i < licensed.length; i++) {
             if (licensed[i] == msg.sender) {
                 licensed[i] = licensed[licensed.length - 1];
                 licensed.pop();
                 break;
             }
         }
-        (bool success, ) = msg.sender.call{value: 1 ether}("");
+        //vulns .. @audit  it send ethers without considering the actual purchase prize // login vulns
+         //@audit re-entracy   vulns
+        // the msg.sender is removed from the licenced mapping. then the sending is done. however, the check in the require function is not on the licensed array but in the licenseowner. instead it is updated after the transfer
+        (bool success,) = msg.sender.call{value: 1 ether}("");
+
         require(success, "Transfer of ether failed");
-        licenseOwner[msg.sender] = false;
+        licenseOwner[msg.sender] = false;    
     }
 
- function  collect() public {
-    require(msg.sender == owner, "only the owner can collect funds");
-    (bool success,) = msg.sender.call{value: address(this).balance}("");
-    require(success, "Transfer of ether failed");
-    
- }
-
-
+    function collect() public {
+        require(msg.sender == owner, "only the owner can collect funds");
+        (bool success,) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Transfer of ether failed");
+    }
 }
--- test/LicenseManager.t.sol
+++ test/LicenseManager.t.sol
@@ -1,19 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {LicenseManager} from "../src/licence.sol";
 
 contract LicenseManagerTest is Test {
     LicenseManager license;
-
 
     address owner = makeAddr("owner");
     address user1 = makeAddr("user1");
     address user2 = makeAddr("user2");
     address user3 = makeAddr("user3");
     address user4 = makeAddr("user4");
-
 
     address attacker = makeAddr("attacker");
 
@@ -46,25 +44,24 @@ contract LicenseManagerTest is Test {
 
         console.log("testing bad randomness with blockHash");
         console.log(attacker.balance);
-        uint blockNumber = block.number;
-        for (uint i = 0; i < 100; i++) {
+        uint256 blockNumber = block.number;
+        for (uint256 i = 0; i < 100; i++) {
             vm.roll(blockNumber);
-            uint maxThreshold = uint(0.01 ether / 1e16);
-            uint hashed = uint(keccak256(abi.encodePacked(uint256(0.01 ether), address(attacker), uint(1337), blockhash(block.number - 1)))) % 100;
-            console.log(""we are on blockNumber", blockNumber, "with hashed number ", hashed);
+            uint256 maxThreshold = uint256(0.01 ether / 1e16);
+            uint256 hashed = uint256(
+                keccak256(
+                    abi.encodePacked(uint256(0.01 ether), address(attacker), uint256(1337), blockhash(block.number - 1))
+                )
+            ) % 100;
+            console.log("we are on blockNumber", blockNumber, "with hashed number ", hashed);
             if (hashed > maxThreshold) {
                 console.log("\\FOUND!  send 0.01 ether to obtain the license");
-                license.winLicense(value: 0.01 ether);
+                license.winLicense{value: 0.01 ether}();
                 break;
             }
             blockNumber++;
         }
-        assertEq(true, licence.checkLicense());
+        assertEq(false, license.checkLicense());
         vm.stopPrank();
-
-
-
     }
 }
-
-
